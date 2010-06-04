bcb8d1a466eed2e8ab2c06b31d5262122469842d
Kevin Bullock <kbullock@ringworld.org>
2010-06-04 17:34:34 -0500

    fix mq tests

--- test/test_vlad_mercurial_queue.rb
+++ test/test_vlad_mercurial_queue.rb
@@ -6,8 +6,10 @@ class TestVladMercurialQueue < MiniTest::Unit::TestCase
 
   def setup
     @scm = Vlad::MercurialQueue.new
-    set :repository, "http://repo/project"
-    set :deploy_to, "/path/to"
+    set :repository,      "http://repo/project"
+    set :deploy_to,       "/path/to"
+    set(:queue_repo)    { "#{repository}/.hg/patches" }
+    set :queue_revision,  "tip"
   end
 
   def test_checkout
@@ -19,7 +21,7 @@ class TestVladMercurialQueue < MiniTest::Unit::TestCase
                "&& hg pull -R .hg/patches http://repo/project/.hg/patches " \
                "&& hg qpop -a " \
                "&& hg update tip " \
-               "&& hg update -R .hg/patches " \
+               "&& hg update -R .hg/patches tip " \
                "&& hg qpush -a"
 
     assert_equal expected, cmd
@@ -27,7 +29,7 @@ class TestVladMercurialQueue < MiniTest::Unit::TestCase
 
   def test_export
     cmd = @scm.export 'head', '/path/to/release'
-    assert_equal 'hg archive -r tip /path/to/release', cmd
+    assert_equal 'hg archive -r qtip /path/to/release', cmd
   end
 
   def test_revision
@@ -48,7 +50,25 @@ class TestVladMercurialQueue < MiniTest::Unit::TestCase
                "&& hg pull -R .hg/patches http://repo/project-patched " \
                "&& hg qpop -a " \
                "&& hg update tip " \
-               "&& hg update -R .hg/patches " \
+               "&& hg update -R .hg/patches tip " \
+               "&& hg qpush -a"
+
+    assert_equal expected, cmd
+  end
+
+  def test_queue_revision
+    set :queue_revision, "deadbeefd00d"
+
+    # only need to test #checkout
+    cmd = @scm.checkout 'head', '/path/to/scm'
+
+    expected = "if [ ! -d .hg ]; then hg init; fi " \
+               "&& if [ ! -d .hg/patches/.hg ]; then hg qinit -c; fi " \
+               "&& hg pull http://repo/project " \
+               "&& hg pull -R .hg/patches http://repo/project/.hg/patches " \
+               "&& hg qpop -a " \
+               "&& hg update tip " \
+               "&& hg update -R .hg/patches deadbeefd00d " \
                "&& hg qpush -a"
 
     assert_equal expected, cmd
