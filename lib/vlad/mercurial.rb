c1af813f9d39b7ad29c1fc77f42e896ae47d06fd
Kevin Bullock <kbullock@umn.edu>
2011-07-18 11:53:21 -0500

    prepare 2.2.1 release

--- lib/vlad/mercurial.rb
+++ lib/vlad/mercurial.rb
@@ -3,7 +3,7 @@ require 'vlad'
 module Vlad
   class Mercurial
 
-    VERSION = '2.2.0'.freeze
+    VERSION = '2.2.1'.freeze
 
     set :source,        Vlad::Mercurial.new
     set :hg_cmd,        'hg'
