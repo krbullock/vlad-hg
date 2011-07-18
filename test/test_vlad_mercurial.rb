c6543d9a2dec1b93be3d394ab25e1d2029dd4a48
Kevin Bullock <kbullock@umn.edu>
2011-07-18 15:06:21 -0500

    add test for deploy_via

--- test/test_vlad_mercurial.rb
+++ test/test_vlad_mercurial.rb
@@ -27,10 +27,17 @@ class TestVladMercurial < Rake::TestCase
     assert_equal 'hg archive -r default /path/to/release', cmd
   end
 
+  def test_export_via
+    set :deploy_via, :checkout
+    cmd = @scm.export 'default', '/path/to/release'
+    assert_equal 'hg clone /path/to/scm -r default /path/to/release', cmd
+  end
+
   def test_export_subrepos
     set :hg_subrepos, true
     cmd = @scm.export 'default', '/path/to/release'
     assert_equal 'hg archive -S -r default /path/to/release', cmd
+    set :hg_subrepos, false
   end
 
   def test_revision
